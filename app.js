d15a83ffaa28eca64d3d1776871fde07d6081faf
raphaelle <raflanelle@hotmail.com>
2024-07-03 10:00:34 +0200

    frontend avec le bon background

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 35e9d0ed37f14beeddcb953ba4dcd5c7901eb3c1
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< HEAD
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-=======
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
@@ -25,7 +13,6 @@ require('./models/connection');
 var app = express();
 
 app.use(cors());
->>>>>>> 35e9d0ed37f14beeddcb953ba4dcd5c7901eb3c1
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,11 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-<<<<<<< HEAD
-app.use('/users', usersRouter);
-=======
 app.use('/trips', tripsRouter);
 app.use('/carts', cartsRouter);
->>>>>>> 35e9d0ed37f14beeddcb953ba4dcd5c7901eb3c1
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-=======
 const fetch = require('node-fetch');
 const Trip = require('../models/trips');
 
@@ -18,4 +10,3 @@ module.exports = router;
 
 
 // || trips.arrival === null
->>>>>>> 35e9d0ed37f14beeddcb953ba4dcd5c7901eb3c1
